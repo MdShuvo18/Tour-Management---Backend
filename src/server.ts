1a0926458c7c2cad437ea3486a8300cee2b23830
MdShuvo18 <mdks33881@gmail.com>
2025-10-16 16:43:09 +0600

    server running to the Port 5000 successfully

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import { Server } from "http";
 import express from "express";
 import mongoose from "mongoose"
+import type Request = require("express");
+import type Response = require("express");
 
 
 let server: Server;
@@ -11,7 +13,7 @@ const startServer = async () => {
         await mongoose.connect("mongodb+srv://todoList-backend:uDcD7NJYbyWh1jDB@cluster0.ameizfp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
         console.log("Connected to the DB")
 
-       server = app.listen(5000, () => {
+        server = app.listen(5000, () => {
             console.log("Server is listing the Port 5000")
         })
     } catch (error) {
@@ -20,3 +22,10 @@ const startServer = async () => {
 }
 
 startServer()
+
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({
+    message: "Welcome to server backend system"
+});
+
+})
