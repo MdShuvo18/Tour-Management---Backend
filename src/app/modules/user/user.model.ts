70f3dcaa72292d9e55b138e4f33d76d147f1b8be
MdShuvo18 <mdks33881@gmail.com>
2025-10-27 10:36:16 +0600

    User Schema Model created

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,42 @@
+import { Schema } from "mongoose";
+import { IsActive, Role, type IAuthProvider, type Iuser } from "./user.interface.js";
+
+
+
+const authProviderSchema = new Schema<IAuthProvider>(
+    {
+        provider: { type: String, required: true },
+        providerId: { type: String, required: true }
+    },
+    { versionKey: false, _id: false })
+
+
+const userSchema = new Schema<Iuser>({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String },
+    role: {
+        type: String,
+        enum: Object.values(Role),
+        default: Role.USER
+    },
+    phone: { type: String },
+    picture: { type: String },
+    address: { type: String },
+    isDeleted: { type: Boolean, default: false },
+    isActive: {
+        type: String,
+        enum: Object.values(IsActive),
+        default: IsActive.ACTIVE,
+    },
+    isVerified: { type: Boolean, default: false },
+    auth: [authProviderSchema],
+
+},
+
+
+    {
+        timestamps: true,
+        versionKey: false
+
+    })
