77e6eb08428d4812434b82a3cf2ef3d4b676e660
MdShuvo18 <mdks33881@gmail.com>
2025-10-27 11:59:55 +0600

    User controllers create done

---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,28 @@
+import type { Request, Response } from "express";
+import { User } from "./user.model.js";
+
+const createUser = async (req: Request, res: Response) => {
+    try {
+        const { name, email } = req.body;
+
+        const user = await User.create({
+            name,
+            email
+        })
+
+        res.status(201).json({
+            message: "User create successfuly done"
+        })
+
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (err: any) {
+        console.log(err);
+        res.status(400).json({
+            message: `Something went wrong!!! ${err.message}`
+        })
+    }
+}
+
+export const userControllers = {
+    createUser
+}
--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,4 +1,4 @@
-import { Schema } from "mongoose";
+import { model, Schema } from "mongoose";
 import { IsActive, Role, type IAuthProvider, type Iuser } from "./user.interface.js";
 
 
@@ -40,3 +40,7 @@ const userSchema = new Schema<Iuser>({
         versionKey: false
 
     })
+
+export const User = model<Iuser>("User",userSchema)    
+
+    
